ee0f48a0a6cf0cafcef716ac4e2ef5a049e7cc53
Luan Carlos Sousa Santos <contato@luancarlos.com.br>
2019-02-21 00:31:48 -0300

    Ínicio protótipo tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatCardModule, MatDividerModule} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatCardModule,
+    MatDividerModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
